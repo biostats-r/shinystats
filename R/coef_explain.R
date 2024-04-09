661cf812bf3a707f242fd796957aeecb291a412d
RichardJTelford <Richard.Telford@uib.no>
2024-04-09 17:12:47 +0200

    arrows

--- R/coef_explain.R
+++ R/coef_explain.R
@@ -1,14 +1,15 @@
 #' Coef-explain app
 #' @import shiny
 #' @import bslib
-#' @importFrom graphics stripchart points
+#' @importFrom graphics stripchart points arrows
 #' @importFrom stats lm coef predict as.formula rnorm
 #' @export
 
 coef_explain_app <- function() {
   ui <- page_sidebar(
     sidebar = sidebar(
-      checkboxGroupInput("pred", "Predictor", c("Food", "Temperature", "Interaction"))
+      checkboxGroupInput("pred", "Predictor", choiceValues = c("Food", "Temperature", "Interaction"),
+                         choiceNames = c("Food (A vs. B)", "Temperature (Low vs. High)", "Interaction"))
     ),
     card(
       card_header("Model formula"),
@@ -16,11 +17,18 @@ coef_explain_app <- function() {
     ),
     card(
       card_header("Coefficients"),
-      tableOutput("table")
+      tableOutput("coef_table")
     ),
     card(
       plotOutput("plot")
-    )
+    ),
+    tags$head(tags$style("#coef_table td{
+                     position:relative;
+                     };
+
+                     ")),
+
+
   )
 
 
@@ -31,9 +39,9 @@ coef_explain_app <- function() {
       b1 <- 3
       b2 <- 2
       b3 <- 2
-      food <- rep(c("A", "B"), each = 24) |> factor()
-      temperature <- rep(c("low", "high"), times = 24) |> factor(levels = c("low", "high"))
-      response <- b0 + b1 * (food == "B") + b2 * (temperature == "high") + b3 * (food == "B" & temperature == "high") + rnorm(48)
+      food <- rep(c("A", "B"), times = 24) |> factor()
+      temperature <- rep(c("Low", "High"), each = 24) |> factor(levels = c("Low", "High"))
+      response <- b0 + b1 * (food == "B") + b2 * (temperature == "High") + b3 * (food == "B" & temperature == "High") + rnorm(48)
       data.frame(food = food, temperature = temperature, response = response)
     })
     form <- "response ~"
@@ -51,28 +59,51 @@ coef_explain_app <- function() {
       }
     })
 
+    observe({
+      if ("Interaction" %in% input$pred){
+        updateCheckboxGroupInput(session, "pred",
+                                 selected = c("Food", "Temperature", "Interaction")
+        )
+     }
+    })
+
     model <- reactive({
       lm(form2(), data = data())
     })
-    table <- reactive({
+
+    coefs <- reactive({
+      coef(model())
+    })
+
+    coef_colours <- reactive({
+      c(`(Intercept)` = "skyblue", foodB = "red", temperatureHigh = "green", `foodB:temperatureHigh` = "orange")[names(coefs())]
+    })
+
+    coef_table <- reactive({
+      c1 <- '<div style="width: 100%; height: 100%; z-index: 0; background-color: '
+      c2 <- '; position:absolute; top: 0; left: 0; padding:5px;">\n<span>'
+      c3 <- '</span></div>'
+
       tab <- data.frame(
-        Beta = paste0("\u03B2", seq_along(coef(model()))),
-        Coefficents = names(coef(model())),
-        Estimates = coef(model())
+        Beta = paste0(c1, coef_colours(), c2, "\u03B2", seq_along(coefs()), c3),
+        Coefficent = names(coefs()),
+        Estimate = coefs()
       )
       tab
     })
 
+
     output$formula <- renderText(form2())
-    output$table <- renderTable(table())
+    output$coef_table <- renderTable(coef_table(), sanitize.text.function = function(x) x)
     output$plot <- renderPlot({
       set.seed(1)
       f <- as.formula(form2())
       fc <- as.character(f)
+      ylim <- c(0, max(data()$response))
       if (fc[3] == "1") {
-        stripchart(data()$response, method = "jitter", jitter = 0.1, vertical = TRUE, pch = 1)
+        stripchart(data()$response, method = "jitter", jitter = 0.1, vertical = TRUE, pch = 1, ylim = ylim)
       } else {
-        stripchart(f, data = data(), method = "jitter", jitter = 0.1, vertical = TRUE, pch = 1)
+        stripchart(f, data = data(), method = "jitter", jitter = 0.1, vertical = TRUE, pch = 1, ylim = ylim)
       }
 
       cols <- c("food", "temperature")[c(grepl("food", fc[3]), grepl("temperature", fc[3]))]
@@ -81,6 +112,37 @@ coef_explain_app <- function() {
       }
       pred <- predict(model(), newdata = unique(data()[, cols, drop = FALSE]))
       points(seq_along(pred), pred, col = "#832424", pch = 16, cex = 4)
+#browser()
+      # add arrows for betas
+      xs <- seq_along(pred) - 0.2
+      # b0
+      arrows(xs, rep(0, length(pred)), xs, rep(coefs()[1], length(pred)), col = coef_colours()[1], lwd = 2, length = 0.1)
+
+      # food main effect
+      pos <- 2 - 0.2 # x position
+      if ("foodB" %in% names(coefs()) ){
+        arrows(pos, coefs()[1], pos, coefs()[1] + coefs()["foodB"], col = coef_colours()["foodB"], lwd = 2, length = 0.1)
+
+        pos <- pos + 1 # move to next position
+
+      }
+
+      if ("temperatureHigh" %in% names(coefs()) ){
+        arrows(pos, coefs()[1], pos, coefs()[1] + coefs()["temperatureHigh"], col = coef_colours()["temperatureHigh"], lwd = 2, length = 0.1)
+        pos <- pos + 1
+
+      }
+
+      if (all(c("foodB", "temperatureHigh") %in% names(coefs()))){
+        arrows(pos, coefs()[1], pos, coefs()[1] + coefs()["foodB"], col = coef_colours()["foodB"], lwd = 2, length = 0.1)
+
+        arrows(pos, coefs()[1] + coefs()["foodB"], pos, coefs()[1] + coefs()["foodB"] + coefs()["temperatureHigh"], col = coef_colours()["temperatureHigh"], lwd = 2, length = 0.1)
+
+        if ("foodB:temperatureHigh" %in% names(coefs())) { # interaction
+          arrows(pos, coefs()[1] + coefs()["foodB"] + coefs()["temperatureHigh"], pos, coefs()[1] + coefs()["foodB"] + coefs()["temperatureHigh"] + coefs()["temperatureHigh"], col = coef_colours()["foodB:temperatureHigh"], lwd = 2, length = 0.1)
+        }
+      }
+
     })
   }
 
